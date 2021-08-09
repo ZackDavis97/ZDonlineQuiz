f77d8fc41a33746f8803bbf0edce26b77ae91c5a
Zack Davis <jantrezac95@gmail.com>
2021-08-09 18:39:41 -0500

    Second commit

---
+++ script.js
@@ -0,0 +1,44 @@
+var quizQuestions = [{
+    question: "What is the color of an orange?",
+    choiceA: "Yes",
+    choiceB: "Orange-ish",
+    choiceC: "Dog",
+    choiceD: "Not orange",
+    correctAnswer: "B"},
+    {
+    question: "What is the answer to life?",
+    choiceA: "Not this answer",
+    choiceB: "Maybe this answer",
+    choiceC: "42",
+    choiceD: "Definitely not this answer",
+    correctAnswer: "C"},
+    {
+    question: "Is yes no ,or, no yes?",
+    choiceA: "What?",
+    choiceB: "Cucumber",
+    choiceC: "yes no",
+    choiceD: "no yes",
+    correctAnswer: "A"},
+    {
+    question: "This is a gimme, the answer is A.",
+    choiceA: "This is the answer",
+    choiceB: "Not this one",
+    choiceC: "Try again",
+    choiceD: "Come on, the answer is A",
+    correctAnswer: "A"},
+    {
+    question: "Who is mans best friend?",
+    choiceA: "Dolphin",
+    choiceB: "Cat",
+    choiceC: "Mr. Potato",
+    choiceD: "Dog",
+    correctAnswer: "D"},
+    {
+    question: "What sound does a duck make?",
+    choiceA: "Bark",
+    choiceB: "Squawk",
+    choiceC: "Moo",
+    choiceD: "Duck sounds",
+    correctAnswer: "D"},
+
+];
